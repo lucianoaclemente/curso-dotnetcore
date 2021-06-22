4334d8fc6803dd6d67c4f89d48dba37a871be0c5
Luciano Clemente <lucianoaclemente@gmail.com>
2021-06-22 15:45:08 -0300

    Atualizações

---
+++ GameTOP/Jogador2.cs
@@ -0,0 +1,20 @@
+namespace GameTOP
+{
+    public class Jogador2 : iJogador
+    {
+        public string Chuta()
+        {
+            return "Maradona chuta\n";
+        }
+
+        public string Corre()
+        {
+            return "Maradona corre\n";
+        }
+
+        public string Passe()
+        {
+            return "Maradona passa\n";
+        }
+    }
+}
