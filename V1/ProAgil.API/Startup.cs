2236126ccf679e4112720affc47f6a1d9bd0afa9
Luciano Clemente <lucianoaclemente@gmail.com>
2021-08-11 14:40:12 -0300

    Atualização

--- V1/ProAgil.API/Startup.cs
+++ V1/ProAgil.API/Startup.cs
@@ -33,6 +33,7 @@ namespace ProAgil.API
             });
             
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +50,8 @@ namespace ProAgil.API
             }
 
             //app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseMvc();            
         }
     }
 }
